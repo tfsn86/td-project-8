cd3233b4532f8a0762c7e0b897ee2894122827c8
tfsn <tfsn86@gmail.com>
2019-12-24 12:54:52 +0100

    Create sequelize book model

---
+++ models/book.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+// Initializes the book sequelize model (NOTE: Add further validation to the year property)
+module.exports = sequelize => {
+	class Book extends Sequelize.Model {}
+	Book.init(
+		{
+			title: {
+				type: Sequelize.STRING,
+				validate: { notEmpty: { msg: '"Title" is required' } }
+			},
+			author: {
+				type: Sequelize.STRING,
+				validate: { notEmpty: { msg: '"Author" is required"' } }
+			},
+			genre: {
+				type: Sequelize.STRING,
+				validate: { notEmpty: { msg: '"Genre" is required"' } }
+			},
+			year: {
+				type: Sequelize.INTEGER,
+				validate: { notEmpty: { msg: '"Year" is required"' } }
+			}
+		},
+		{ sequelize }
+	);
+};
