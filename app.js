acc2c1889035695658cfce02a28dc3b27a5a6f76
tfsn <tfsn86@gmail.com>
2019-12-27 17:56:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const routes = require('./routes/index');
+const books = require('./routes/books');
 
 const app = express();
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/books', books);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+	next(createError(404));
+});
 
 app.listen(3000, () => {
 	console.log('Listening on localhost 3000');
