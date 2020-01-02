14b463878b8984c85411c9a2de2d5a9813fb446e
tfsn <tfsn86@gmail.com>
2020-01-02 17:24:18 +0100

    Routing for error handling updated

--- app.js
+++ app.js
@@ -18,13 +18,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.locals.basedir = app.get('views');
 
 app.use('/', routes);
 app.use('/books', books);
 
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
 	next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	res.status(err.status || 500);
+	res.render('page-not-found');
 });
 
 app.listen(3000, () => {
